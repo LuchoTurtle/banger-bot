6a7559e50e10167996450115b99af487901c4e59
Luís Arteiro <lucho14garfield@gmail.com>
2023-07-16 19:14:57 +0100

    chore: Fixing requirements.txt conflicts

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,9 @@ import asyncio
 
 import pytest
 from decouple import config
-from pyrogram import Client
 
-from tgintegration import BotController
+from pyrogram import Client
+# from tgintegration import BotController
 
 import subprocess
 import time
@@ -13,7 +13,6 @@ from src.definitions.definitions import SRC_DIR
 #coverage run --omit="*/tests*" --source=. -m pytest tests/unit -s
 #https://stackoverflow.com/questions/63623930/how-to-create-unit-test-for-a-python-telegram-bot
 # Para o main, criar mock Updater e Context para testar o main como se fosse um unit test
-
 
 @pytest.fixture(scope="session")
 def run_bot():
@@ -61,14 +60,18 @@ async def client() -> Client:
     await client.stop()
 
 
-@pytest.fixture(scope="module")
-async def controller(client, event_loop, run_bot):
-    c = BotController(
-        peer="@banger_music_bot",  # We are going to run tests on https://t.me/banger_music_bot
-        client=client,
-        max_wait=8,  # Maximum timeout for responses (optional)
-        wait_consecutive=2,  # Minimum time to wait for more/consecutive messages (optional)
-        raise_no_response=True,  # Raise `InvalidResponseError` when no response received (defaults to True)
-        global_action_delay=2.5,  # Choosing a rather high delay so we can follow along in realtime (optional)
-    )
-    yield c
+# This piece of code was using `tgintegration` for integration testing.
+# Because there are conflicts with the requirements, integration testing has been disabled.
+# So this fixture is not used.
+# 
+# @pytest.fixture(scope="module")
+# async def controller(client, event_loop, run_bot):
+#     c = BotController(
+#         peer="@banger_music_bot",  # We are going to run tests on https://t.me/banger_music_bot
+#         client=client,
+#         max_wait=8,  # Maximum timeout for responses (optional)
+#         wait_consecutive=2,  # Minimum time to wait for more/consecutive messages (optional)
+#         raise_no_response=True,  # Raise `InvalidResponseError` when no response received (defaults to True)
+#         global_action_delay=2.5,  # Choosing a rather high delay so we can follow along in realtime (optional)
+#     )
+#     yield c
